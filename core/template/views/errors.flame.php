d2f0152380bac62d8e9613dd057d4850f94902b9
Martin Binder <72764328+bndrmrtn@users.noreply.github.com>
2023-05-18 19:20:17 +0200

    Updated errors page

--- core/template/views/errors.flame.php
+++ core/template/views/errors.flame.php
@@ -1,20 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title }} - {{ _env('NAME','Flame Core') }}</title>
-    {{ stylesheet('css/bootstrap.min.css') }}
-    {{ stylesheet('css/demo.css') }}
-</head>
-<body>
-    <div class="ftext">
-        <h1>{{ $code }} - {{ $title }}</h1>
-        <p>{{ $message }}</p>
-    </div>
+@extends:.src/:assets/app;
+
+@yield:main;
+    <h1 class="text-3xl">{{ $code }} - {{ $title }}</h1>
+    <p>{{ $message }}</p>
     @if(_env('APP_DEV')):
-    <div class="bottom">Render time: {{ getrtime() }}s, Debug: <b>{{ $trace['file'] }}:{{ $trace['line'] }}</b></div>
+        <p>Render time: {{ getrtime() }}s,<br/>Debug: <b>{{ $trace['file'] }}:{{ $trace['line'] }}</b></p>
     @endif
-</body>
-</html>
+@endyield:main;
